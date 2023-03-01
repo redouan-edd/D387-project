e573666804886cb7c2c5fe454e65251633047917
redouan-edd <55621869+redouan-edd@users.noreply.github.com>
2023-03-01 23:58:09 +0100

    Update H2Bootstrap.java
    changes

--- src/main/java/edu/wgu/d387_sample_code/H2Bootstrap.java
+++ src/main/java/edu/wgu/d387_sample_code/H2Bootstrap.java
@@ -1,19 +1,28 @@
 package edu.wgu.d387_sample_code;
 
+import edu.wgu.d387_sample_code.entity.ReservationEntity;
 import edu.wgu.d387_sample_code.entity.RoomEntity;
+import edu.wgu.d387_sample_code.repository.ReservationRepository;
 import edu.wgu.d387_sample_code.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
+@Transactional
 public class H2Bootstrap implements CommandLineRunner {
 
 	@Autowired
 	RoomRepository roomRepository;
-	
+	@Autowired
+	ReservationRepository reservationRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
@@ -21,20 +30,14 @@ public class H2Bootstrap implements CommandLineRunner {
 		System.out.println("Bootstrapping data: ");
 		RoomEntity room1= new RoomEntity(405, "200");
 		room1.setId(1L);
-		
+		LocalDate checkinDate1 = LocalDate.of(2023, 3, 10);
+		LocalDate checkoutDate1 = LocalDate.of(2023, 3, 14);
+		ReservationEntity reservation1 = new ReservationEntity(checkinDate1, checkoutDate1, room1);
+		reservation1.setId(1L);
+		reservationRepository.save(reservation1);
+		room1.addReservationEntity(reservation1);
 		roomRepository.save(room1);
 
-		RoomEntity room2= new RoomEntity(406, "220");
-		room2.setId(2L);
-
-		roomRepository.save(room2);
-
-		RoomEntity room3= new RoomEntity(407, "260");
-		room3.setId(3L);
-
-		roomRepository.save(room3);
-
-		
 		Iterable<RoomEntity> itr = roomRepository.findAll();
 		
 		System.out.println("Printing out data: ");
